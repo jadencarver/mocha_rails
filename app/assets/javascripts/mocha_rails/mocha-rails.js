80d700405b9fd29d1adaec0e7bb0d3285efe9762
Jaden Carver <jaden.carver@gmail.com>
2013-04-05 20:08:36 -0400

    mocha-phantomjs support

--- app/assets/javascripts/mocha_rails/mocha-rails.js
+++ app/assets/javascripts/mocha_rails/mocha-rails.js
@@ -2,6 +2,11 @@ mocha.setup('bdd');
 chai.should(); // Adds should method to Object's prototype.
 expect = chai.expect;
 onload = function() {
-  var runner = mocha.run();
+  var runner;
+  if (window.mochaPhantomJS) {
+    runner = mochaPhantomJS.run();
+  } else {
+    runner = mocha.run();
+  }
   //runner.globals(['testExports']);
 };
